2ed8502e5f0d9a5482a2dd4a173c53d882971773
elysonlaroa-web <elysonlaroa@gmail.com>
2026-03-11 11:42:32 +0800

    Rename departments table to department

--- database/migrations/2026_03_08_061352_create_department_table.php
+++ database/migrations/2026_03_08_061352_create_department_table.php
@@ -1,23 +1,24 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('departments', function (Blueprint $table) {
+        Schema::create('department', function (Blueprint $table) {
             $table->id('department_id');
             $table->string('department_name');
             $table->string('department_email')->nullable();
 
-            $table->softDeletes(); // deleted_at
-            $table->timestamps();  // created_at & updated_at
+            $table->softDeletes(); 
+            $table->timestamps(); 
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('departments');
+        Schema::dropIfExists('department');
     }
 };
